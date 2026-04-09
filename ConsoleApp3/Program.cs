85829893c202c432a45e9f33c254849980050a80
BlackSerperior6 <m330.nek76@gmail.com>
2026-04-09 11:15:51 +0500

    Morron less

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,12 +2,40 @@
 using ScottPlot;
 
 Random random = new Random(23);
-/*List<int> results = new List<int>();
 
-for (int i = 0; i < 101; i++)
-    results.Add(random.Next(0, 2));
+Dictionary<AntState, List<(double time, double duration, AntState end)>> simulation(double maxTime, Dictionary<AntState, Dictionary<AntState, double>> states, 
+    out double accumulatedTime)
+{
+    accumulatedTime = 0;
 
-Console.WriteLine(string.Join(" ", results));*/
+    Dictionary<AntState, List<(double time, double duration, AntState end)>> results = 
+    new Dictionary<AntState, List<(double time, double duration, AntState end)>>();
+
+    AntState currentState = AntState.StealBread;
+
+    foreach (var key in states.Keys)
+        results[key] = new List<(double time, double duration, AntState end)>();
+
+    while (accumulatedTime <= maxTime)
+    {
+        Dictionary<AntState, double> chances = new Dictionary<AntState, double>();
+
+        foreach (var pair in states[currentState])
+        {
+            var roll = -1 / (double) pair.Value * Math.Log(random.NextDouble());
+            chances.Add(pair.Key, roll);
+        }
+
+        var min = chances.OrderByDescending(x => x.Value).Last();
+
+        results[currentState].Add(new (accumulatedTime, min.Value, min.Key));
+
+        currentState = min.Key;
+        accumulatedTime += min.Value; 
+    }
+
+    return results;
+}
 
 Dictionary<AntState, Dictionary<AntState, double>> States = new Dictionary<AntState, Dictionary<AntState, double>>() 
 {
@@ -37,14 +65,8 @@ Dictionary<AntState, Dictionary<AntState, double>> States = new Dictionary<AntSt
     } },
 };
 
-int levelCounter = 10;
-
+double levelCounter = 0.1;
 Dictionary<string, double> yLevels = new Dictionary<string, double>();
-Dictionary<AntState, List<(double time, double duration, AntState end)>> list = new Dictionary<AntState, List<(double time, 
-    double duration, AntState end)>>();
-
-foreach (var key in States.Keys)
-    list[key] = new List<(double time, double duration, AntState end)>();
 
 foreach (var key in States.Keys)
 {
@@ -52,81 +74,62 @@ foreach (var key in States.Keys)
     levelCounter++;
 }
 
-double currentTime = 0;
+var modelOneDay = simulation(24, States, out var accumlatedTime);
 
-AntState currentState = AntState.StealBread;
-
-Dictionary<double, List<double>> mediums = new Dictionary<double, List<double>>();
-
-for (int i = 1; i <= 1000; i++)
+/*using (var plt = new Plot())
 {
-    Dictionary<AntState, double> chances = new Dictionary<AntState, double>();
-
-    foreach (var pair in States[currentState])
+    double[] x = times.ToArray();
+    int colorsCounter = -1;
+    
+    foreach(var pair in mediumTimes)
     {
-        var roll = -1 / (double) pair.Value * Math.Log(random.NextDouble());
-        chances.Add(pair.Key, roll);
+        colorsCounter++;
+    
+        if (colorsCounter >= Colors.Category10.Length)
+            colorsCounter = 0;
+        
+        var color = Colors.Category10[colorsCounter];
+
+        double[] y = pair.Value.ToArray();
+
+        var scatter = plt.Add.Scatter(x, y);
+        scatter.Color = color;
+        scatter.LineWidth = 2;
     }
 
-    var min = chances.OrderByDescending(x => x.Value).Last();
-
-    list[currentState].Add(new (currentTime, min.Value, min.Key));
-
-    currentState = min.Key;
-    currentTime += min.Value;
-
-    if (i % 10 == 0)
-    {
-        Dictionary<AntState, double> mediumTimes = new Dictionary<AntState, double>();
-
-        foreach (var pair in list)
-        {
-            var timeSum = list.Sum(x => x.Value.Sum(v => v.duration));
-            var medium = timeSum / currentTime;
-
-            mediumTimes.Add(pair.Key, medium);
-        }
-
-        mediums[currentTime] = new List<double>();
-
-        foreach (var pair in mediumTimes)
-            mediums[currentTime].Add(pair.Value);
-    }
-}
+    plt.SaveSvg("AntsAvaragePlot.svg", 2000, 4000);
+}*/
 
 using (var plt = new Plot())
 {
-    foreach (var pair in list)
+    foreach (var pair in modelOneDay)
     {
-        var colorCounter = 0;
 
         foreach (var value in pair.Value)
         {
-            colorCounter++;
-
-            if (colorCounter >= Colors.Category10.Length)
-                colorCounter = 0;
-
-            var color = Colors.Category10[colorCounter];
-
             var scatter = plt.Add.Scatter(value.time, yLevels[pair.Key.ToString()]);
 
             var progressArrow = plt.Add.Arrow(new Coordinates(value.time, yLevels[pair.Key.ToString()]),
                             new Coordinates(value.time + value.duration, yLevels[pair.Key.ToString()]));
-
-            progressArrow.ArrowFillColor = color;
-            progressArrow.ArrowLineColor = color;
+            
+            progressArrow.ArrowFillColor = Colors.Black;
+            progressArrow.ArrowLineColor = Colors.Black;
 
             var loweringArrow = plt.Add.Arrow(new Coordinates(value.time + value.duration, yLevels[pair.Key.ToString()]),
                             new Coordinates(value.time + value.duration, yLevels[value.end.ToString()]));
-
-            loweringArrow.ArrowFillColor = color;
-            loweringArrow.ArrowLineColor = color;
+            
+            loweringArrow.ArrowFillColor = Colors.Black;
+            loweringArrow.ArrowLineColor = Colors.Black;
         }
     }
 
-    plt.SaveSvg("AntsMovementsPlot.svg", 2000, 4000);
+    plt.Axes.Left.SetTicks(yLevels.Values.ToArray(), yLevels.Keys.ToArray());
+    plt.Axes.Bottom.TickGenerator = new ScottPlot.TickGenerators.NumericFixedInterval(0.1);
+    plt.Axes.SetLimitsX(0, 24);
+    plt.SaveSvg("AntsMovementsPlot.svg", 16000, 500);
 }
+
+
 
 
 
